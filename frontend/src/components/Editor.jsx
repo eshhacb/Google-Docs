bb893635308b809e1cfb4bdb05cbd30116c445d5
Esha Raj <eshhacb@gmail.com>
2025-03-03 15:10:02 +0530

    Editor added

--- frontend/src/components/Editor.jsx
+++ frontend/src/components/Editor.jsx
@@ -4,26 +4,34 @@ import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import axios from "axios";
 
-const socket = io("http://localhost:5003");
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+const socket = io(API_BASE_URL, { transports: ["websocket"] });
 
 const Editor = ({ documentId }) => {
   const [content, setContent] = useState("");
 
   useEffect(() => {
+    if (!documentId) return;
+
     socket.emit("join-document", documentId);
 
-    socket.on("document-updated", (newContent) => {
+    const handleDocumentUpdate = (newContent) => {
       setContent(newContent);
-    });
+    };
+
+    socket.on("document-updated", handleDocumentUpdate);
 
     axios
-      .get(`http://localhost:5003/api/collaboration/${documentId}`)
+      .get(`${API_BASE_URL}/api/collaboration/${documentId}`)
       .then((res) => {
-        setContent(res.data.content);
-      });
+        setContent(res.data.content || "");
+      })
+      .catch((err) => console.error("Error fetching document:", err));
 
     return () => {
-      socket.off("document-updated");
+      socket.off("document-updated", handleDocumentUpdate);
+      socket.emit("leave-document", documentId);
     };
   }, [documentId]);
 
