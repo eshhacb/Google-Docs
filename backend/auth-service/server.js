32cd645784d4bfb68ec6ff114d2e9afe1fc103a9
Esha Raj <eshhacb@gmail.com>
2025-03-05 15:49:08 +0530

    Changes made

--- backend/auth-service/server.js
+++ backend/auth-service/server.js
@@ -9,12 +9,12 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-// app.use(
-//   cors({
-//     origin: ["http://localhost:5173", "http://localhost:3000"],
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:3000"],
+    credentials: true,
+  })
+);
   app.use(cookieParser());
 // Routes
 app.use("/api/auth", authRoutes);
